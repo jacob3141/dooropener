fe59616318b936c08442fa4c8199266d3dc2b455
Sascha Schwabbauer <sascha@evolved.io>
2014-12-22 13:31:29 +0100

    Initial project setup

---
+++ swift/DOAR/DOAR OS X App/Main App/Delegates/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DOAR
+//
+//  Created by Sascha Schwabbauer on 22/12/14.
+//  Copyright (c) 2014 evolved.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
