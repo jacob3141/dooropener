ece2ae353a89ab9de3b79ecc6355f64840734b8b
Malte Baumann <malte.baumann@9elements.com>
2014-12-22 15:55:09 +0100

    Added basic GUI

--- swift/DOAR/DOAR iOS App/Main App/View Controllers/ViewController.swift
+++ swift/DOAR/DOAR iOS App/Main App/View Controllers/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var openDoorButton: UIButton!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    var connected: Bool = false {
+        didSet {
+            openDoorButton.enabled = connected;
+            statusLabel.text = (connected) ? "Connected" : "Disconnected"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,21 @@ class ViewController: UIViewController {
     }
 
 
+    /// MARK: Actions
+    
+    @IBAction func didTapOpenDoorButton(sender: UIButton) {
+        openDoor()
+    }
+    
+    /// MARK: Door handling
+    
+    private func openDoor() {
+        self.activityIndicator.startAnimating()
+        
+        // TODO: Actually open door
+        
+        self.activityIndicator.stopAnimating()
+    }
+    
 }
 
