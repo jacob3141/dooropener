5f5bbdc0e8736afa9f07cf9cac98030286a6be67
Malte Baumann <malte.baumann@9elements.com>
2014-12-22 16:32:34 +0100

    Added ConnectionController handling

--- swift/DOAR/DOAR iOS App/Main App/View Controllers/ViewController.swift
+++ swift/DOAR/DOAR iOS App/Main App/View Controllers/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Starscream
+import DOARKitiOS
 
 class ViewController: UIViewController {
     
     @IBOutlet var statusLabel: UILabel!
     @IBOutlet var openDoorButton: UIButton!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    var connectionController = ConnectionController()
     
     var connected: Bool = false {
         didSet {
@@ -23,13 +27,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /// MARK: Notifications
+    
+    private func registerForNotifications() {
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "connectionStateDidChange", name: AppConfiguration.Notifications.ConnectionStateDidChangeNotification, object: nil)
+    }
+    
+    private func unregisterFromNotifications() {
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "connectionStateDidChange", name: AppConfiguration.Notifications.ConnectionDidReceiveDoorRingNotification, object: nil)
+    }
 
+    private func connectionStateDidChange() {
+        switch (self.connectionController.state) {
+        case .Closed:
+            NSLog.("Connection closed", nil...)
+        case .Failed:
+            NSLog.("Connection failed", nil...)
+        case .Open:
+            NSLog.("Connection opened", nil...)
+        }
+    }
+    
+    private func doorRingReceived() {
+        
+    }
 
     /// MARK: Actions
     
